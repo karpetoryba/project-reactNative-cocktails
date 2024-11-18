b28393ccaff94a9efbeb1f2cdcb1afc07d8d3ca4
karpetoryba <karpenkokarpenko24@gmail.com>
2024-11-18 12:40:37 +0100

    new design for card

--- cocktails-esd/app/index.tsx
+++ cocktails-esd/app/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.card}>
-        <Text style={styles.title}> Coctails </Text>
+        <Text style={styles.title}> Coktails </Text>
         <Text style={styles.soustext}>Les meilleurs coctails de Bordeaux:</Text>
         <Button title="Voir tous les coctails" />
         <Text style={styles.centretext}>
@@ -12,7 +12,7 @@ export default function Index() {
           ingrédients, généralement des alcools, des jus de fruits, des sodas,
           des sirops, et parfois même des épices ou des herbes aromatiques.{" "}
         </Text>
-        <Text style={styles.title}> Les meilleurs coctails: </Text>
+        <Text style={styles.maintitle}> Les meilleurs coctails: </Text>
         <Text style={styles.cocktail}>
           1. Mojito - Rhum, menthe, sucre, citron vert, eau gazeuse
         </Text>
@@ -35,49 +35,66 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    backgroundColor: "#FAFAFA",
+    padding: 20,
   },
   card: {
-    backgroundColor: "#f0f0f0",
+    backgroundColor: "#ffffff",
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 10,
+    borderRadius: 12,
+    padding: 15,
     shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 3,
-    elevation: 4,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 5,
+    marginVertical: 10,
+    width: "90%",
   },
-
   title: {
-    fontSize: 50,
-    color: "orange",
+    fontSize: 36,
+    color: "#FF5722",
     fontWeight: "bold",
-    margin: 20,
+    marginBottom: 10,
+  },
+  maintitle: {
+    fontSize: 25,
+    color: "#FF5722",
+    fontWeight: "bold",
+    marginBottom: 10,
   },
   soustext: {
-    fontSize: 10,
-    fontWeight: "bold",
-    margin: 20,
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 15,
+    color: "#555",
+    textAlign: "center",
   },
   centretext: {
-    margin: 20,
+    marginBottom: 20,
+    fontSize: 14,
+    color: "#444",
+    textAlign: "center",
   },
   cocktail: {
-    fontSize: 10,
-    fontWeight: "bold",
+    fontSize: 16,
     color: "#333",
+    textAlign: "center",
+    marginBottom: 10,
   },
   footer: {
     position: "absolute",
     bottom: 0,
     width: "100%",
-    padding: 10,
-    backgroundColor: "#333",
+    padding: 15,
+    backgroundColor: "#FF5722",
     alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: "#fff",
-    fontSize: 14,
+    color: "#ffffff",
+    fontSize: 12,
+    fontWeight: "500",
   },
 });
